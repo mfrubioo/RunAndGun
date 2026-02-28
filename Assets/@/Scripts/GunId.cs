3592f241cfda18b81584c56a3931eb093ac59459
Unai <unairubiocarrion@gmail.com>
2026-02-28 03:57:32 +0100

    PickUp

---
+++ Assets/@/Scripts/GunId.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class GunId : MonoBehaviour
+{
+    public int id;
+}
---
+++ Assets/@/Scripts/PickUp.cs
@@ -0,0 +1,114 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    PlayerInputActions inputActions;
+    List<string> gunNames = new List<string>()
+    {
+        "rifle","escopeta", "fusil de precision","glock","desert eagle"
+    };
+
+    Camera cam;
+
+    [Header("Raycast")]
+    float rayDistance=2f;
+    public LayerMask pickUpObjects;
+    TextMeshProUGUI txtPickUp;
+
+    bool textUpdate=false;
+    public bool playerCam=true;
+
+    private void Awake()
+    {
+        SetUpInputActions();
+        SetUpCamera();
+        SetUpUI();
+    }
+
+    void SetUpInputActions()
+    {
+        inputActions=new PlayerInputActions();
+        inputActions.Player.PickUp.performed+=ctx=> PickUpAction();
+    }
+
+    void SetUpCamera()
+    {
+        cam=GameObject.Find("PlayerCamera").GetComponent<Camera>();
+    }
+
+    void SetUpUI()
+    {
+        txtPickUp=(GameObject.Find("PickUpTxt").GetComponent<TextMeshProUGUI>());
+        txtPickUp.gameObject.SetActive(false);
+        
+    }
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void Update()
+    {
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, rayDistance, pickUpObjects))
+        {
+            if (!textUpdate)
+            {
+                txtPickUp.gameObject.SetActive(true);
+                PickUpTXTUpdate(hit.collider.gameObject);
+                textUpdate = true;
+            }            
+        }
+        else
+        {
+            txtPickUp.gameObject.SetActive(false);
+            textUpdate = false;
+        }
+
+    }
+
+    void PickUpTXTUpdate(GameObject gun)
+    {
+        int gunID = gun.GetComponent<GunId>().id;
+        txtPickUp.text = "Pulsa E para coger " + gunNames[gunID];
+    }
+
+    void PickUpAction()
+    {
+        if (!playerCam) return;
+        Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, rayDistance,pickUpObjects))
+        {
+            GunIdentificator(hit.collider.gameObject);
+        }
+    }
+
+    void GunIdentificator(GameObject go)
+    {
+        Destroy(go);
+        int _id = go.GetComponent<GunId>().id;
+        if (go.tag == "ArmaPrincipal")
+        {
+            //acceder a la lista de armas principales con el id
+        }
+        else if (go.tag == "ArmaSecundaria")
+        {
+            //acceder a la lista de armas secundaria con el id
+        }
+        else return;
+    }
+
+}
